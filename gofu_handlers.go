5a937f368a6daa91388c225a055ba1c72614c551
Sho Kusano <sho-kusano@cookpad.com>
2013-09-08 20:26:35 +0900

    Refine image handler

--- gofu_handlers.go
+++ gofu_handlers.go
@@ -12,6 +12,26 @@ import (
   "os"
 )
 
+type CropContext struct {
+  width uint
+  height uint
+  x int
+  y int
+}
+
+type RequestContext struct {
+  mtime int64
+  width uint
+  height uint
+  quarity uint
+  blur float64
+  crop *CropContext
+}
+
+func (ctx *RequestContext) isResize() bool {
+  return (ctx.width > 0 && ctx.height > 0)
+}
+
 func bench(label string, f func()) {
   t := time.Now().UnixNano()
   for(len(label) < 30) {
@@ -21,6 +41,14 @@ func bench(label string, f func()) {
   if gofuConfig.Verbose {
     fmt.Printf("%s: %d\n", label, (time.Now().UnixNano() - t)/1000000)
   }
+}
+
+func atoi64(query string, source int64) int64 {
+  to, err := strconv.ParseInt(query, 10, 32)
+  if err == nil {
+    source = to
+  }
+  return source
 }
 
 func atoi(query string, source int) int {
@@ -47,11 +75,11 @@ func atof(query string, source float64) float64 {
   return source
 }
 
-func (server *GofuServer) getImage(reqPath string) (p string, e error) {
+func (server *GofuServer) getImage(reqPath string, context *RequestContext) (p string, e error) {
   cachePath := path.Join(gofuConfig.Dir.Cache, reqPath)
 
   fileInfo, err := os.Stat(cachePath)
-  if err != nil {
+  if err != nil || fileInfo.ModTime().Unix() < context.mtime {
     blob, err := server.bucket.Get(reqPath)
 
     if err != nil {
@@ -63,56 +91,61 @@ func (server *GofuServer) getImage(reqPath string) (p string, e error) {
     io, _ := os.Create(cachePath)
     defer io.Close()
     io.Write(blob)
-  } else {
-    fmt.Println(fileInfo.ModTime().Unix())
   }
 
   return cachePath, err
 }
 
-func (server *GofuServer) processImage(mw *imagick.MagickWand, filePath string, query url.Values) {
-  originWidth := mw.GetImageWidth()
-  originHeight := mw.GetImageHeight()
-  width := originWidth
-  height := originHeight
-  blur := gofuConfig.Image.DefaultBlur
-  quarity := gofuConfig.Image.DefaultQuarity
+func (server *GofuServer) parseQuery(query url.Values, context *RequestContext) {
+  context.blur = gofuConfig.Image.DefaultBlur
+  context.quarity = gofuConfig.Image.DefaultQuarity
 
+  if query["m"] != nil {
+    context.mtime = atoi64(query["m"][0], context.mtime)
+  }
   if query["w"] != nil {
-    width = atoui(query["w"][0], width)
+    context.width = atoui(query["w"][0], context.width)
   }
   if query["h"] != nil {
-    height = atoui(query["h"][0], height)
+    context.height = atoui(query["h"][0], context.height)
   }
   if query["q"] != nil {
-    quarity = atoui(query["q"][0], quarity)
+    context.quarity = atoui(query["q"][0], context.quarity)
   }
   if query["b"] != nil {
-    blur = atof(query["b"][0], blur)
+    context.blur = atof(query["b"][0], context.blur)
   }
+  if query["c"] != nil {
+    cropQuery := strings.Split(query["c"][0], ",")
+    if len(cropQuery[0]) > 0 && len(cropQuery[1]) > 0 && len(cropQuery[2]) > 0 && len(cropQuery[3]) > 0 {
+      context.crop = new(CropContext)
+      context.crop.width = atoui(cropQuery[0], context.width)
+      context.crop.height = atoui(cropQuery[1], context.height)
+      context.crop.x = atoi(cropQuery[2], 0)
+      context.crop.y = atoi(cropQuery[3], 0)
+    }
+  }
+}
 
-  mw.SetOption("jpeg:size", fmt.Sprintf("%dx%d", width, height))
+func (server *GofuServer) processImage(mw *imagick.MagickWand, filePath string, ctx *RequestContext) {
+  if ctx.isResize() {
+    mw.SetOption("jpeg:size", fmt.Sprintf("%dx%d", ctx.width, ctx.height))
+  }
 
   bench("Read Image", func() {
     mw.ReadImage(filePath)
   })
 
-  if query["c"] != nil {
-    cropQuery := strings.Split(query["c"][0], ",")
-    if len(cropQuery[0]) > 0 && len(cropQuery[1]) > 0 && len(cropQuery[2]) > 0 && len(cropQuery[3]) > 0 {
-      cropWidth := atoui(cropQuery[0], width)
-      cropHeight := atoui(cropQuery[1], height)
-      cropPointX := atoi(cropQuery[2], 0)
-      cropPointY := atoi(cropQuery[3], 0)
-      mw.CropImage(cropWidth, cropHeight, cropPointX, cropPointY)
-    }
+  if ctx.crop != nil {
+    crop := ctx.crop
+    mw.CropImage(crop.width, crop.height, crop.x, crop.y)
   }
 
-  if width != originWidth || height != originHeight {
-    mw.ResizeImage(width, height, imagick.FILTER_CUBIC, blur)
+  if ctx.isResize() {
+    mw.ResizeImage(ctx.width, ctx.height, imagick.FILTER_CUBIC, ctx.blur)
   }
 
-  mw.SetImageCompressionQuality(quarity)
+  mw.SetImageCompressionQuality(ctx.quarity)
   mw.StripImage()
 }
 
@@ -120,8 +153,11 @@ func (server *GofuServer) imageHandler(res *GofuResponse, req *http.Request) {
   var filePath string
   var err error
 
+  ctx := new(RequestContext)
+  server.parseQuery(req.URL.Query(), ctx)
+
   bench("Load by S3 or Cache", func() {
-    filePath, err = server.getImage(req.URL.Path[1:])
+    filePath, err = server.getImage(req.URL.Path[1:], ctx)
     if err != nil {
       res.Status = http.StatusNotFound
       res.ClearBody()
@@ -139,7 +175,7 @@ func (server *GofuServer) imageHandler(res *GofuResponse, req *http.Request) {
   }()
 
   bench("ProcessImage", func() {
-    server.processImage(magickWand, filePath, req.URL.Query())
+    server.processImage(magickWand, filePath, ctx)
   })
 
   bench("Write Response", func() {
