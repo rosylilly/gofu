5cc7635b1933e96eba0db3e9b0916c1020c77099
suin <suinyeze@gmail.com>
2014-08-19 17:25:32 +0900

    Fix conflict

--- gofu/bench.go
+++ gofu/bench.go
@@ -1,16 +1,6 @@
 package gofu
 
 import (
-<<<<<<< HEAD
-  "fmt"
-  "time"
-)
-
-func bench(label string, f func()) {
-  n := time.Now()
-  f()
-  fmt.Println(label, ":", time.Now().Sub(n))
-=======
   "time"
   "fmt"
 )
@@ -19,5 +9,4 @@ func bench(label string, f func()) {
   now := time.Now()
   f()
   fmt.Println(label, ":", time.Now().Sub(now))
->>>>>>> Fixes
 }
